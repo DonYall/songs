b2fe80e43da5b8e03226dd251ccf35248c91b97e
DonYall <75219427+DonYall@users.noreply.github.com>
2023-03-21 21:44:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,115 @@
+// Get a reference to the search box
+const searchBox = document.getElementById('search-box');
+
+// Add an event listener to the search box
+searchBox.addEventListener('input', search);
+
+// Define the search function
 function search() {
-  var input = document.getElementById("search-box").value;
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-      var result = xhr.responseText;
-      var lines = result.split('\n');
-      var output = '';
-      for (var i = 0; i < lines.length; i++) {
-        var cols = lines[i].split(',');
-        if (cols[0] === input) {
-          var values = cols[1].replace(/[\[\]']+/g, '').split(', ');
-          output += '<ul>';
-          for (var j = 0; j < values.length; j++) {
-            if (values[j].includes("youtube.com")) {
-              var videoId = values[j].split('v=')[1];
-              output += '<li><iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>';
-            } else {
-              output += '<li>' + values[j] + '</li>';
-            }
-          }
-          output += '</ul>';
-        }
-      }
-      document.getElementById('results').innerHTML = output || '<p>No results found.</p>';
-    }
-  };
-  xhr.open('GET', 'data.csv');
-  xhr.send();
+    // Get the search term from the search box
+    const searchTerm = searchBox.value.trim().toLowerCase();
+
+    // Do the search and display the results
+    const results = searchSpreadsheet(searchTerm);
+    displayResults(results);
 }
+
+// Define the searchSpreadsheet function
+function searchSpreadsheet(searchTerm) {
+    // Get a reference to the spreadsheet data
+    const data = spreadsheetData;
+
+    // Filter the data based on the search term
+    const filteredData = data.filter(row => row[0].toLowerCase().includes(searchTerm));
+
+    // Map the filtered data to an array of objects
+    const mappedData = filteredData.map(row => {
+        const values = row[1].replace(/[\[\]']+/g, '').split(', ');
+        const videos = values.filter(value => value.includes('youtube.com')).map(value => {
+            const videoId = value.split('v=')[1];
+            return {
+                type: 'video',
+                url: `https://www.youtube.com/embed/${videoId}`,
+                title: `Video - ${row[0]}`,
+                description: `Video result for search term "${searchTerm}"`
+            }
+        });
+        const otherValues = values.filter(value => !value.includes('youtube.com')).map(value => {
+            return {
+                type: 'text',
+                content: value
+            }
+        });
+        const items = [...videos, ...otherValues];
+        return {
+            key: row[0],
+            items: items
+        }
+    });
+
+    // Return the mapped data
+    return mappedData;
+}
+
+// Define the displayResults function
+function displayResults(results) {
+    // Get a reference to the results container
+    const resultsContainer = document.getElementById('results');
+
+    // Clear the results container
+    resultsContainer.innerHTML = '';
+
+    // If there are no results, display a message
+    if (results.length === 0) {
+        resultsContainer.innerHTML = '<p>No results found.</p>';
+        return;
+    }
+
+    // Loop through each result and display it
+    results.forEach(result => {
+        // Create a container for the result
+        const resultContainer = document.createElement('div');
+        resultContainer.classList.add('result');
+
+        // Create a heading for the result
+        const heading = document.createElement('h2');
+        heading.textContent = result.key;
+        resultContainer.appendChild(heading);
+
+        // Create a container for the items
+        const itemsContainer = document.createElement('div');
+        itemsContainer.classList.add('items');
+        resultContainer.appendChild(itemsContainer);
+
+        // Loop through each item and display it
+        result.items.forEach(item => {
+            if (item.type === 'video') {
+                // Create an iframe for the video
+                const iframe = document.createElement('iframe');
+                iframe.setAttribute('src', item.url);
+                iframe.setAttribute('width', '560');
+                iframe.setAttribute('height', '315');
+                iframe.setAttribute('frameborder', '0');
+                iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+                iframe.setAttribute('allowfullscreen', '');
+                itemsContainer.appendChild(iframe);
+            } else if (item.type === 'text') {
+                // Create a paragraph for the text
+                const paragraph = document.createElement('p');
+                paragraph.textContent = item.content;
+                itemsContainer.appendChild(paragraph);
+            }
+        });
+    });
+
+    // Attach event listener to the search button
+    searchButton.addEventListener('click', search);
+
+    // Attach event listener to the enter key
+    searchBox.addEventListener('keydown', event => {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            searchButton.click();
+        }
+    });
+};
