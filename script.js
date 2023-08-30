a56673f9ae54e3da5b9fd80acf3cc9a59d862259
DonYall <75219427+DonYall@users.noreply.github.com>
2023-08-29 20:16:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
-function getData(callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            const result = xhr.responseText;
-            callback(result);
-        }
-    };
-    xhr.open('GET', 'data.csv');
-    xhr.send();
-}
-
 function search() {
     const input = document.getElementById('search-box').value;
     getData(result => {
         const lines = result.split('\n');
-        let output = '';
+        let videos = [];
         for (let i = 0; i < lines.length; i++) {
             const cols = lines[i].split(',');
             if (cols[0] === input) {
                 const values = cols[1].replace(/""/g, '"').replace(/[\[\]']/g, '').split(', ');
-                output += '<ul>';
                 for (let j = 0; j < values.length; j++) {
                     if (values[j].includes('youtube.com')) {
                         const videoId = values[j].split('v=')[1];
-                        output += `<li><iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>`;
-                    } else {
-                        output += `<li>${values[j]}</li>`;
+                        videos.push(videoId);
                     }
                 }
-                output += '</ul>';
             }
         }
-        document.getElementById('results').innerHTML = output || '<p>No results found.</p>';
+
+        if (videos.length > 0) {
+            const videoIds = videos.join(',');
+            const embedCode = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoIds}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            document.getElementById('results').innerHTML = embedCode;
+        } else {
+            document.getElementById('results').innerHTML = '<p>No results found.</p>';
+        }
     });
 }
