8bace1f8b82058cbd43a2315d1ed4916b02debca
DonYall <75219427+DonYall@users.noreply.github.com>
2023-08-29 20:19:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,23 @@ function search() {
     const input = document.getElementById('search-box').value;
     getData(result => {
         const lines = result.split('\n');
-        let videos = [];
+        let output = '';
         for (let i = 0; i < lines.length; i++) {
             const cols = lines[i].split(',');
             if (cols[0] === input) {
                 const values = cols[1].replace(/""/g, '"').replace(/[\[\]']/g, '').split(', ');
+                output += '<ul>';
                 for (let j = 0; j < values.length; j++) {
                     if (values[j].includes('youtube.com')) {
                         const videoId = values[j].split('v=')[1];
-                        videos.push(videoId);
+                        output += `<li><iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>`;
+                    } else {
+                        output += `<li>${values[j]}</li>`;
                     }
                 }
+                output += '</ul>';
             }
         }
-
-        if (videos.length > 0) {
-            const videoIds = videos.join(',');
-            const embedCode = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoIds}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-            document.getElementById('results').innerHTML = embedCode;
-        } else {
-            document.getElementById('results').innerHTML = '<p>No results found.</p>';
-        }
+        document.getElementById('results').innerHTML = output || '<p>No results found.</p>';
     });
 }
